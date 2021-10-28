2e9198147292009543a99dfd10cc780be8c37dd3
kenna <kenna.hwa@gmail.com>
2021-10-28 17:53:42 +0900

    node로 크롤링

--- crawlling_copy.py
+++ crawlling_copy.py
@@ -1,15 +1,16 @@
 import requests;
 from bs4 import BeautifulSoup;
 
-resp = requests.get("https://search.naver.com/search.naver?query=강남구+날씨")
+resp = requests.get("https://bvoat.com/myshop/buy_records/list_all.html")
 if resp.status_code == 200:
    	html = resp.text
 
+print(resp.status_code)
 
 #HTML안에 위 내용이 들어가 있다고 가정.
-soup = BeautifulSoup(html,"html.parser")
+#soup = BeautifulSoup(html,"html.parser")
 
-box = soup.find("div",{"class":"temperature_text"}).text
+#box = soup.find("div",{"class":"cont_content"}).text
 
 
-print(box)
+
